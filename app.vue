bb363a75bb3095fe60ed2a0b4305d177d13bee4f
AdnanMadi417 <164775717+AdnanMadi417@users.noreply.github.com>
2024-11-22 14:31:54 +0800

    Fix: Add vue-cli-service and build script

---
+++ app.vue
@@ -0,0 +1,14 @@
+<template>
+  <NuxtPage/>
+</template>
+
+
+<style >
+:root {
+  --main-color: #31363F;
+  --main-hovor-color: #222831;
+  --font-color: #EEEEEE;
+  --font-hovor-color: #F5F5F5;
+  --button-color: #39701d;
+}
+</style>
